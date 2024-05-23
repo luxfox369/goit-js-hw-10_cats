92c28ab1eba84f8bb6fc0618610181c82cb5b017
luxfox369 <113688325+luxfox369@users.noreply.github.com>
2024-05-23 17:00:42 +0000

    Deploying to gh-pages from @ luxfox369/goit-js-hw-10_cats@edd7051adc0b15d5105e3e28259481b77d3a4654 🚀

--- src/refs.js
+++
@@ -1,6 +0,0 @@
-export const refs = {
-    select: document.querySelector(".breed-select"),
-    info:   document.querySelector(".cat-info"),
-    loader: document.querySelector(".loader"),
-    error : document.querySelector(".error"),
-}
